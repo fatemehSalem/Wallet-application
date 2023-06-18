224fc74b2a7aabdaa020e7b2e8ae501b2c397e73
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2023-06-18 08:35:22 +0330

    -add balance_after_transaction to response

--- account/src/main/kotlin/com/micro/account/entity/response/TransactionResponse.kt
+++ account/src/main/kotlin/com/micro/account/entity/response/TransactionResponse.kt
@@ -6,6 +6,7 @@ import java.time.LocalDateTime
 data class TransactionResponse (
     val transaction_id: String?,
     val tx_base_amount: BigDecimal?,
+    val balance_after_transaction: String?,
     val to_account_number: String?,
     val to_account_name: String?,
     val from_account_name: String?,
--- account/src/main/kotlin/com/micro/account/utils/GeneralUtils.kt
+++ account/src/main/kotlin/com/micro/account/utils/GeneralUtils.kt
@@ -4,20 +4,15 @@ import com.micro.account.entity.Results
 import com.micro.account.entity.model.Payload
 import com.micro.account.entity.response.GeneralResponse
 import com.micro.account.entity.response.TransactionResponse
+import org.json.JSONObject
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.web.client.RestTemplate
+import java.math.BigDecimal
 import kotlin.random.Random
 
 object GeneralUtils {
-/*    fun generateRandomString(): String {
-        val length = Random.nextInt(8, 11)
-        val chars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
-        return (1..length)
-            .map { chars.random() }
-            .joinToString("")
-    }*/
 
     fun splitLastFourDigits(phoneNumber: String): String {
         val lastIndex = phoneNumber.length - 1
@@ -46,29 +41,41 @@ object GeneralUtils {
     fun createTransactionHistoryResponseList(resultsList: List<Results>): List<TransactionResponse> {
         return resultsList.map { results ->
             TransactionResponse(
-                    transaction_id = results.id,
-                    tx_base_amount = results.tx_base_amount,
-                    to_account_number = results.to_account_number,
-                    to_account_name = results.to_description,
-                    from_account_name = results.from_description,
-                    transaction_type_id = results.transaction_type_id,
-                    transaction_type = results.transaction_type,
-                    completed_date_utc = results.completed_date_utc
+                transaction_id = results.id,
+                tx_base_amount = results.tx_base_amount,
+                to_account_number = results.to_account_number,
+                balance_after_transaction = results.tx_additional_data_json?.let { getPaymentAvailable(it) },
+                to_account_name = results.to_description,
+                from_account_name = results.from_description,
+                transaction_type_id = results.transaction_type_id,
+                transaction_type = results.transaction_type,
+                completed_date_utc = results.completed_date_utc
             )
         }
     }
 
     fun createTransactionDetailResponse(result: Results): TransactionResponse {
         return TransactionResponse(
-                transaction_id = result.id,
-                tx_base_amount = result.tx_base_amount,
-                to_account_number = result.to_account_number,
-                to_account_name = result.to_description,
-                from_account_name = result.from_description,
-                transaction_type_id = result.transaction_type_id,
-                transaction_type = result.transaction_type,
-                completed_date_utc = result.completed_date_utc
+            transaction_id = result.id,
+            tx_base_amount = result.tx_base_amount,
+            to_account_number = result.to_account_number,
+            balance_after_transaction = result.tx_additional_data_json?.let { getPaymentAvailable(it) },
+            to_account_name = result.to_description,
+            from_account_name = result.from_description,
+            transaction_type_id = result.transaction_type_id,
+            transaction_type = result.transaction_type,
+            completed_date_utc = result.completed_date_utc
         )
     }
+
+    private fun getPaymentAvailable(jsonString: String): String {
+        val jsonObject = JSONObject(jsonString)
+        val balanceChanges = jsonObject.getJSONObject("BalanceChanges")
+        val receiver = balanceChanges.getJSONObject("Receiver")
+        val after = receiver.getJSONObject("After")
+        val paymentAvailable = after.getBigDecimal("PaymentAvailable") ?: BigDecimal.ZERO
+        return paymentAvailable.toString()
+    }
+
 
 }
